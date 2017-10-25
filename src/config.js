00aec273958f943ab9d12c3867cd3f042806d29c
Corentin Dupont <corentin.dupont@gmail.com>
2017-10-25 15:12:00 +0200

    use local backend

--- src/config.js
+++ src/config.js
@@ -7,10 +7,10 @@ module.exports = {
   // API URL to be used in the server-side code
   serverUrl: process.env.SERVER_URL || `http://localhost:${process.env.SERVER_PORT || 3000}`,
 
-  keycloakUrl: process.env.KEYCLOAK_URL || 'http://aam.waziup.io/auth',
+  keycloakUrl: process.env.KEYCLOAK_URL || 'http://localhost:8080/auth',
   realm: 'waziup',
   clientId: 'api-server',
-  orionUrl: process.env.ORION_URL || 'http://broker.waziup.io',
-  elasticsearchUrl: process.env.ELASTICSEARCH_URL || 'http://elasticsearch.waziup.io',
+  orionUrl: process.env.ORION_URL || 'http://localhost:1026',
+  elasticsearchUrl: process.env.ELASTICSEARCH_URL || 'http://localhost:9200',
   mongoDBUrl: process.env.MONGODB_URL || 'mongodb://localhost:27017/waziup_history'
 };
