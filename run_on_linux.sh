a03d8b555879155121475ba39752de4599e0b42e
Johann Forster <j-forster@users.noreply.github.com>
2017-11-25 23:12:14 +0100

    HTTPS support
    added https sopport and a cert&key for localhost

---
+++ run_on_linux.sh
@@ -0,0 +1,11 @@
+export HTTP_ENABLED="true"
+export HTTP_PORT="3000"
+export HTTP_URL="http://localhost:3000"
+
+export HTTPS_ENABLED="true"
+export HTTPS_PORT="3001"
+export HTTPS_TLS_CRT="tls/localhost.crt"
+export HTTPS_TLS_KEY="tls/localhost.key"
+export HTTPS_URL="https://localhost:3001"
+
+npm start
---
+++ run_on_windows.bat
@@ -0,0 +1,11 @@
+set HTTP_ENABLED=true
+set HTTP_PORT=3000
+set HTTP_URL=http://localhost:3000
+
+set HTTPS_ENABLED=true
+set HTTPS_PORT=3001
+set HTTPS_TLS_CRT=tls/localhost.crt
+set HTTPS_TLS_KEY=tls/localhost.key
+set HTTPS_URL=https://localhost:3001
+
+npm start
