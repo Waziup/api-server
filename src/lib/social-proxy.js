eefc33ee8a618cc72e675e4bbda53ff206ace4cb
Corentin Dupont <corentin.dupont@gmail.com>
2018-01-18 11:32:48 +0100

    socials

--- src/lib/social-proxy.js
+++ src/lib/social-proxy.js
@@ -6,8 +6,6 @@ const config = require('../config.js');
 const users = require('../routes/users/user.route.js');
 const axios = require('axios');
 
-const token = 'eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJ1M0cxOXg4U2YxWHlnb2RUYjdJbU91NDVsS1NRNUF0MV8yRXJvWVZOZlk4In0.eyJqdGkiOiJlMzI4MTg4Zi1iODFmLTQ0NTktYjVlNi05MjRkY2JkYzg5NDUiLCJleHAiOjE1MDk5ODQzNTMsIm5iZiI6MCwiaWF0IjoxNTA5MjkzMTUzLCJpc3MiOiJodHRwOi8vbG9jYWxob3N0OjgwODAvYXV0aC9yZWFsbXMvbWFzdGVyIiwiYXVkIjoiYWRtaW4tY2xpIiwic3ViIjoiYjlhZTBmMmYtNzgwMy00Zjk5LThiODAtMjBkOTFkNzVmNTYwIiwidHlwIjoiQmVhcmVyIiwiYXpwIjoiYWRtaW4tY2xpIiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiMGNiNzA2ZTAtZTVmYi00NGJlLWE2MzMtMzA3ODIyNjk5MjQyIiwiYWNyIjoiMSIsImNsaWVudF9zZXNzaW9uIjoiYWI5YWI5OTQtZDI3Ni00N2EwLTljMjAtZjQ3YjZhZTUwMmNlIiwiYWxsb3dlZC1vcmlnaW5zIjpbIioiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbImNyZWF0ZS1yZWFsbSIsImFkbWluIl19LCJyZXNvdXJjZV9hY2Nlc3MiOnsid2F0ZXJzZW5zZS1yZWFsbSI6eyJyb2xlcyI6WyJ2aWV3LWlkZW50aXR5LXByb3ZpZGVycyIsInZpZXctcmVhbG0iLCJtYW5hZ2UtaWRlbnRpdHktcHJvdmlkZXJzIiwiaW1wZXJzb25hdGlvbiIsImNyZWF0ZS1jbGllbnQiLCJtYW5hZ2UtdXNlcnMiLCJ2aWV3LWF1dGhvcml6YXRpb24iLCJtYW5hZ2UtZXZlbnRzIiwibWFuYWdlLXJlYWxtIiwidmlldy1ldmVudHMiLCJ2aWV3LXVzZXJzIiwidmlldy1jbGllbnRzIiwibWFuYWdlLWF1dGhvcml6YXRpb24iLCJtYW5hZ2UtY2xpZW50cyJdfSwid2F6aXVwLXJlYWxtIjp7InJvbGVzIjpbInZpZXctcmVhbG0iLCJ2aWV3LWlkZW50aXR5LXByb3ZpZGVycyIsIm1hbmFnZS1pZGVudGl0eS1wcm92aWRlcnMiLCJpbXBlcnNvbmF0aW9uIiwiY3JlYXRlLWNsaWVudCIsIm1hbmFnZS11c2VycyIsInZpZXctYXV0aG9yaXphdGlvbiIsIm1hbmFnZS1ldmVudHMiLCJtYW5hZ2UtcmVhbG0iLCJ2aWV3LWV2ZW50cyIsInZpZXctdXNlcnMiLCJ2aWV3LWNsaWVudHMiLCJtYW5hZ2UtYXV0aG9yaXphdGlvbiIsIm1hbmFnZS1jbGllbnRzIl19LCJtYXN0ZXItcmVhbG0iOnsicm9sZXMiOlsidmlldy1pZGVudGl0eS1wcm92aWRlcnMiLCJ2aWV3LXJlYWxtIiwibWFuYWdlLWlkZW50aXR5LXByb3ZpZGVycyIsImltcGVyc29uYXRpb24iLCJjcmVhdGUtY2xpZW50IiwibWFuYWdlLXVzZXJzIiwidmlldy1hdXRob3JpemF0aW9uIiwibWFuYWdlLWV2ZW50cyIsIm1hbmFnZS1yZWFsbSIsInZpZXctZXZlbnRzIiwidmlldy11c2VycyIsInZpZXctY2xpZW50cyIsIm1hbmFnZS1hdXRob3JpemF0aW9uIiwibWFuYWdlLWNsaWVudHMiXX19LCJuYW1lIjoiIiwicHJlZmVycmVkX3VzZXJuYW1lIjoiYWRtaW4ifQ.nYu6pDDjdYSszSrXwc0y2PaUEfF7LliHuR9HPNTfBrtViITdVlqFLLtrFthCXt31Mol7TYZuZk1vjJP-yIlaMuSJl75DzQnB2tOpDu2-TKEBwruhJIBh-VxNQW28oQLTuP50JWq1pKUWMnK_ObGOteI3k-U-66ekcw3H9bAa-3Sw8xMhGV8qZ228aPOvhvqvKe-gHEsGx-UTpPCdOMBFJyZc3smBC84It971YyrgvV1uXaJomTNOsUSkkem_UjTn5aze3GwnKSkwdelW46AW87uwsPtt9n8qIrwe-mXoQN-DOvTOapBmq4pNQucx-Hujn_yQWqvwRBID4lgYXddq9g'
-
 //get all messages
 async function getSocialMsgs(domain) {
   return socialRequest('', 'GET', null)
@@ -57,7 +55,7 @@ async function postSocialMsgBatch(domain, socialMsgBatch) {
 //Perform a request on the social backend
 async function socialRequest(path, method, data) {
  
-    var url = config.backend.socialsUrl + '/SocialBackend/socials/' + path;
+    var url = config.backend.socialsUrl + '/api/v1/domains/waziup/socials/' + path;
     var axiosConf = {method: method,
                      url: url,
                      data: data}
@@ -72,27 +70,25 @@ async function socialRequest(path, method, data) {
 // ## Helper functions ##
 
 function getMsg(usr, channel, message) {
-  if(!usr.attributes) {
-    throw('no user attributes')
-  }
+  var userID = {}
   switch(channel.toLowerCase()) {
       case 'facebook':
-          if(usr.attributes.facebook) {
-             userID = usr.attributes.facebook[0]; 
+          if(usr.facebook) {
+             userID = usr.facebook[0]; 
           } else {
             throw('Facebook user attribute not defined')
           }
           break;
       case 'twitter':
-          if(usr.attributes.twitter) {
-             userID = usr.attributes.twitter[0]; 
+          if(usr.twitter) {
+             userID = usr.twitter[0]; 
           } else {
             throw('Twitter user attribute not defined')
           }
           break;
       case 'sms':
-          if(usr.attributes.phone) {
-             userID = usr.attributes.phone[0];
+          if(usr.phone) {
+             userID = usr.phone[0];
           } else {
             throw('phone user attribute not defined')
           }
